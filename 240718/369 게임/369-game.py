1a0e7afba9ac459165b2735a464f3ec2a7beeb50
coldrain-f <intmain__@naver.com>
2024-07-18 20:03:27 +0900

    Title: 369 게임, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/369 게임/369-game.py
+++ 240718/369 게임/369-game.py
@@ -3,11 +3,14 @@ n = int(input())
 for i in range(1, n + 1):
     satisfied = False
     m = i
-    while m > 0:
-        digit = m % 10
-        if digit == 3 or digit == 6 or digit == 9:
-            satisfied = True
-        m //= 10
+    if i % 3 == 0:
+        satisfied = True
+    else:
+        while m > 0:
+            digit = m % 10
+            if digit == 3 or digit == 6 or digit == 9:
+                satisfied = True
+            m //= 10
     if satisfied:
         print(0, end=" ")
     else:
