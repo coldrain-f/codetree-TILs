7b6df8640dc11fca70ed2ec314ca4c639b633c57
coldrain-f <intmain__@naver.com>
2024-07-18 20:10:07 +0900

    Title: 369 게임, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/369 게임/369-game.py
+++ 240718/369 게임/369-game.py
@@ -4,7 +4,7 @@ def is369(n: int) -> bool:
     if n % 3 == 0:
         return True
     while n > 0:
-        if n % 10 in {3, 6, 9}:
+        if n % 10 in [3, 6, 9]:
             return True
         n //= 10
     return False
