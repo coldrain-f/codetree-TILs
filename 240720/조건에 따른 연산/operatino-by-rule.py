270e3df1abddd753b6dbf770c962f2979ee9773c
coldrain-f <intmain__@naver.com>
2024-07-20 14:07:34 +0900

    Title: 조건에 따른 연산, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240720/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,13 @@
+n = int(input())
+count = 0
+
+while True:
+    if n >= 1000:
+        print(count)
+        break
+        
+    if n % 2 == 0:
+        n = (n * 3) + 1
+    else:
+        n = (n * 2) + 2
+    count += 1
