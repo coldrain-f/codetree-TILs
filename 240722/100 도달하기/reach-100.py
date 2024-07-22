8d389101f64e430a62266db7b664c9d750ff8f37
coldrain-f <intmain__@naver.com>
2024-07-22 20:38:27 +0900

    Title: 100 도달하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/100 도달하기/reach-100.py
+++ 240722/100 도달하기/reach-100.py
@@ -1,14 +1,11 @@
 n = int(input())
 
-# 넉넉하게 20개 정도로 잡는다.
-arr = [0] * 20
-arr[0] = 1
-arr[1] = n
-
-print(arr[0], arr[1], end=" ")
-
-for i in range(2, 20):
-    arr[i] = arr[i - 2] + arr[i - 1]
-    print(arr[i], end=" ")
+arr = [1, n]
+i = 2
+while True:
+    arr.append(arr[i - 2] + arr[i - 1])
     if arr[i] >= 100:
         break
+    i += 1
+
+print(*arr)
