77780a583366bab53b440155b588d6c342886389
coldrain-f <intmain__@naver.com>
2024-07-22 20:18:03 +0900

    Title: 짝수인 것만 출력하기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240722/짝수인 것만 출력하기/print-even-number.py
+++ 240722/짝수인 것만 출력하기/print-even-number.py
@@ -2,9 +2,5 @@ n = int(input())
 arr = list(map(int, input().split()))
 even = []
 
-# 짝수를 새로운 배열에 저장 후 출력
-for item in arr:
-    if item % 2 == 0:
-        even.append(item)
-
+even = filter(lambda item : item % 2 == 0, arr)
 print(*even)
