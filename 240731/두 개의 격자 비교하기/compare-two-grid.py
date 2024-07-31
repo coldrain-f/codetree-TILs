d88ffe55dcb347007bca45e519ad629991db1eb5
coldrain-f <intmain__@naver.com>
2024-07-31 21:21:12 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240731/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+
+_list1 = [
+    list(map(int, input().split()))
+    for _ in range(m)
+]
+_list2 = [
+    list(map(int, input().split()))
+    for _ in range(m)
+]
+
+for i in range(n):
+    for j in range(m):
+        if _list1[i][j] == _list2[i][j]:
+            print(0, end=" ")
+        else:
+            print(1, end=" ")
+    print()
