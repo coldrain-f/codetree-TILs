9979cccf2a788761f80d587a4839edac58133b1f
coldrain-f <intmain__@naver.com>
2024-08-11 19:14:14 +0900

    Title: 격자 모양 출력하기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240811/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+_list = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for _ in range(m):
+    r, c = map(int, input().split())
+    _list[r - 1][c - 1] = r * c
+
+for i in range(n):
+    for j in range(n):
+        print(_list[i][j], end=" ")
+    print()
